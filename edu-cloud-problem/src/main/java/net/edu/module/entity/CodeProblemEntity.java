860e632c22651995e192466fec6aa34b9bb0a9f1
17716 <1771605689@qq.com>
2022-09-03 19:09:08 +0800

    问题管理模块

--- edu-cloud-problem/src/main/java/net/edu/module/entity/CodeProblemEntity.java
+++ edu-cloud-problem/src/main/java/net/edu/module/entity/CodeProblemEntity.java
@@ -1,7 +1,13 @@
 package net.edu.module.entity;
 
+import lombok.Data;
+
 /**
- *
+ * @Author: 马佳浩
+ * @Date: 2022/9/3 17:35
+ * @Version: 1.0
+ * @Description:
  */
+@Data
 public class CodeProblemEntity {
 }

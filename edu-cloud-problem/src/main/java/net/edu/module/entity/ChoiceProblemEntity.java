a7bfd1217fcde9fe1be734cb7ef00bef0f6611b0
17716 <1771605689@qq.com>
2022-09-06 21:08:22 +0800

    问题管理

---
+++ edu-cloud-problem/src/main/java/net/edu/module/entity/ChoiceProblemEntity.java
@@ -0,0 +1,94 @@
+package net.edu.module.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import net.edu.framework.mybatis.entity.BaseEntity;
+
+/**
+ * 选择题库表
+ *
+ * @author 马佳浩 
+ * @since 1.0.0 2022-09-05
+ */
+@Data
+@EqualsAndHashCode(callSuper=false)
+@TableName("problem_choice")
+public class ChoiceProblemEntity extends BaseEntity {
+
+	/**
+	* 类型
+	*/
+	private Integer type;
+
+	/**
+	* 名称
+	*/
+	private String name;
+
+	/**
+	* 问题说明
+	*/
+	private String description;
+
+	/**
+	* 来源
+	*/
+	private String source;
+
+	/**
+	* 提示
+	*/
+	private String tips;
+
+	/**
+	* 建议用时
+	*/
+	private Integer adviceTime;
+
+	/**
+	* 图片
+	*/
+	private String img;
+
+	/**
+	* 难度
+	*/
+	private Integer difficulty;
+
+	/**
+	* 知识点
+	*/
+	private Long kpId;
+
+	/**
+	* 回答次数
+	*/
+	private Integer submitTimes;
+
+	/**
+	* 正确次数
+	*/
+	private Integer correctTimes;
+
+	/**
+	* 典型问题
+	*/
+	private Integer isTypical;
+
+	/**
+	 * 选项数
+	 */
+	private Integer optionNum;
+
+	/**
+	* 引用次数
+	*/
+	private Integer usedNum;
+
+	/**
+	 * 状态
+	 */
+	private Integer status;
+
+}
---
+++ edu-cloud-problem/src/main/java/net/edu/module/vo/ChoiceProblemVO.java
@@ -0,0 +1,84 @@
+package net.edu.module.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import net.edu.framework.common.utils.DateUtils;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+* 选择题库表
+*
+* @author 马佳浩 
+* @since 1.0.0 2022-09-05
+*/
+@Data
+@Schema(description = "选择题库表")
+public class ChoiceProblemVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Schema(description = "问题ID")
+	private Long id;
+
+	@Schema(description = "类型")
+	private Integer type;
+
+	@Schema(description = "名称")
+	private String name;
+
+	@Schema(description = "问题说明")
+	private String description;
+
+	@Schema(description = "来源")
+	private String source;
+
+	@Schema(description = "提示")
+	private String tips;
+
+	@Schema(description = "建议用时")
+	private Integer adviceTime;
+
+	@Schema(description = "图片")
+	private String img;
+
+	@Schema(description = "难度")
+	private Integer difficulty;
+
+	@Schema(description = "知识点")
+	private Long kpId;
+
+	@Schema(description = "知识点名称")
+	private Integer kpName;
+
+	@Schema(description = "回答次数")
+	private Integer submitTimes;
+
+	@Schema(description = "正确次数")
+	private Integer correctTimes;
+
+	@Schema(description = "典型问题")
+	private Integer isTypical;
+
+
+	@Schema(description = "选项数")
+	private Integer optionNum;
+
+	@Schema(description = "引用次数")
+	private Integer usedNum;
+
+	@Schema(description = "创建时间")
+	@JsonFormat(pattern = DateUtils.DATE_TIME_PATTERN)
+	private Date createTime;
+
+
+	@Schema(description = "创建人")
+	private Integer creator;
+
+
+	@Schema(description = "状态")
+	private Integer status;
+
+
+}

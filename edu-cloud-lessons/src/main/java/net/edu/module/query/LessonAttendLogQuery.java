a9572e44c3ce3beff382152daf56cd0cbce0d7da
17716 <1771605689@qq.com>
2022-09-15 12:39:23 +0800

    创建上课模块

--- edu-cloud-lessons/src/main/java/net/edu/module/query/LessonAttendLogQuery.java
+++ edu-cloud-lessons/src/main/java/net/edu/module/query/LessonAttendLogQuery.java
@@ -8,11 +8,11 @@ import net.edu.framework.common.query.Query;
 import java.util.Date;
 
 /**
-* 课堂签到表查询
-*
-* @author 马佳浩 
-* @since 1.0.0 2022-09-15
-*/
+ * 课堂签到表查询
+ *
+ * @author 马佳浩
+ * @since 1.0.0 2022-09-15
+ */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Schema(description = "课堂签到表查询")

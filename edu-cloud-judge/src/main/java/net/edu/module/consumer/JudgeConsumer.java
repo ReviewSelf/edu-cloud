6515b177e2ac3d57e58b85ddb3da0d19bb7b47c9
17716 <1771605689@qq.com>
2022-09-20 19:08:49 +0800

    学生进入班级校验，生成班级签到表

--- edu-cloud-judge/src/main/java/net/edu/module/consumer/JudgeConsumer.java
+++ edu-cloud-judge/src/main/java/net/edu/module/consumer/JudgeConsumer.java
@@ -1,13 +1,16 @@
 package net.edu.module.consumer;
 
+import com.rabbitmq.client.Channel;
 import lombok.SneakyThrows;
 import net.edu.framework.common.mq.QueueName;
 import net.edu.module.service.JudgeService;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -23,12 +26,18 @@ public class JudgeConsumer {
     JudgeService judgeService;
 
 
+
     @SneakyThrows
-    @RabbitListener(queues = QueueName.JUDGE_QUEUE, concurrency = "3")
+    @RabbitListener(queues = QueueName.JUDGE_QUEUE  ,concurrency = "3")
     @RabbitHandler
-    public void listenerJudge(String recordId) {
-        System.out.println(new Date() + recordId);
-        judgeService.judgeCode(Long.valueOf(recordId));
+    public void listenerJudge(String msg, Channel channel, Message message) {
+        try {
+            judgeService.judgeCode(Long.valueOf(msg));
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (IOException e) {
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            throw new RuntimeException(e);
+        }
     }
 
 

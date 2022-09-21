b1cb285f913703a2969f0630ecb3a168e1e211a4
“zjc” <1419291534@qq.com>
2022-09-21 20:54:46 +0800

    完成题目资源管理

---
+++ edu-cloud-problem/src/main/java/net/edu/module/dao/ProblemResourceDao.java
@@ -0,0 +1,25 @@
+package net.edu.module.dao;
+
+
+import net.edu.framework.mybatis.dao.BaseDao;
+import net.edu.module.entity.ProblemResourceEntity;
+import net.edu.module.vo.ProblemResourceVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+* 问题资源表
+*
+* @author 周建超 
+* @since 1.0.0 2022-09-20
+*/
+@Mapper
+public interface ProblemResourceDao extends BaseDao<ProblemResourceEntity> {
+    List<ProblemResourceVO> selectProblemResource(@Param("id") Long id);
+
+    int deleteProblemResource(@Param("id") Long id);
+
+    int insertProblrmResource(ProblemResourceVO vo);
+}

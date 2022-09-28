a76dd4a54ff5b3f90e4d11393989915220c2587e
17716 <1771605689@qq.com>
2022-09-28 18:32:01 +0800

    调整代码

--- edu-cloud-file/src/main/java/net/edu/module/properties/LocalStorageProperties.java
+++ edu-cloud-file/src/main/java/net/edu/module/properties/LocalStorageProperties.java
@@ -9,23 +9,20 @@ import org.springframework.stereotype.Component;
  *
  */
 @Data
-@Configuration
 @Component
 public class LocalStorageProperties {
-
-
 
     /**
      * 本地存储路径
      */
     @Value("${storage.local.path}")
-    private String path="E://upload";
+    private String path;
 
     @Value("${storage.config.domain}")
-    private String domain="http://127.0.0.1:8080/file";
+    private String domain;
     /**
      * 资源起始路径
      */
     @Value("${storage.config.prefix}")
-    private String url="upload";
+    private String url;
 }

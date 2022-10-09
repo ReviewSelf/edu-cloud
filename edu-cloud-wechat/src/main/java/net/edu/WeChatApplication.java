241934e0bdb840526c040b492054bcd935266b57
skwdsb <414837845@qq.com>
2022-10-09 17:22:42 +0800

    每日更新

--- edu-cloud-wechat/src/main/java/net/edu/WeChatApplication.java
+++ edu-cloud-wechat/src/main/java/net/edu/WeChatApplication.java
@@ -3,12 +3,14 @@ package net.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  *
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class WeChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeChatApplication.class, args);

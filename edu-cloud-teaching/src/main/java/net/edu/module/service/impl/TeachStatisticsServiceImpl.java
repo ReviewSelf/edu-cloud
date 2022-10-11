97036db6187bc13a29f609c9cefe9a62b6c2be56
17716 <1771605689@qq.com>
2022-10-11 09:58:55 +0800

    首页模块

--- edu-cloud-teaching/src/main/java/net/edu/module/service/impl/TeachStatisticsServiceImpl.java
+++ edu-cloud-teaching/src/main/java/net/edu/module/service/impl/TeachStatisticsServiceImpl.java
@@ -1,7 +1,9 @@
 package net.edu.module.service.impl;
 
 import lombok.AllArgsConstructor;
+import net.edu.framework.common.cache.RedisKeys;
 import net.edu.framework.common.utils.DateUtils;
+import net.edu.framework.common.utils.RedisUtils;
 import net.edu.framework.mybatis.service.impl.BaseServiceImpl;
 import net.edu.module.dao.TeachStatisticsDao;
 import net.edu.module.dao.UserDao;
@@ -21,16 +23,19 @@ import java.util.Date;
 public  class TeachStatisticsServiceImpl  implements TeachStatisticsService {
 
     private final TeachStatisticsDao statisticsDao;
+    private final RedisUtils redisUtils;
+
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public TeachStatisticsInfoEntity getStatisticsInfo() {
-        //去除最近一次统计的题库总量、用户总量、问题提交总量以及前一天用户总量
-        TeachStatisticsInfoEntity statisticsInfo = statisticsDao.selectStatisticsInfo();
-        //统计最近30天用户活跃量
-        statisticsInfo.setUserActivityTotal(statisticsDao.selectUserActivity(30));
-        //统计最近7天的每天题目提交趋势
-        statisticsInfo.setProblemSubmitTotal(statisticsDao.selectProblemSubmit(7));
+        TeachStatisticsInfoEntity statisticsInfo=null;
+        statisticsInfo= (TeachStatisticsInfoEntity) redisUtils.get(RedisKeys.getTeachStatistics());
+        if(statisticsInfo==null){
+            //最近一次统计的题库总量、用户总量、问题提交总量以及前一天用户总量//统计最近30天用户活跃量//统计最近7天的每天题目提交趋势
+           statisticsInfo = statisticsDao.selectStatisticsInfo();
+            redisUtils.set(RedisKeys.getTeachStatistics(),statisticsInfo,RedisUtils.HOUR_SIX_EXPIRE);
+        }
         return statisticsInfo;
     }
 
@@ -39,6 +44,7 @@ public  class TeachStatisticsServiceImpl  implements TeachStatisticsService {
     public void statisticsHomeInfo(){
         //统计题库日增量/用户日增量/统计问题提交日增量/用户每天活跃量并将统计结果存入统计表中
         statisticsDao.insertStatisticsInfo();
+        redisUtils.del(RedisKeys.getTeachStatistics());
     }
 
 }

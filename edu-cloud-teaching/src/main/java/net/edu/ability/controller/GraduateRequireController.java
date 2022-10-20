20c517442ef3d912b93bb60810ce3ce5f1d465e5
18758839159 <3225658422@qq.com>
2022-10-20 10:27:25 +0800

    10.20

---
+++ edu-cloud-teaching/src/main/java/net/edu/ability/controller/GraduateRequireController.java
@@ -0,0 +1,69 @@
+package net.edu.ability.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import net.edu.ability.convert.GraduateRequireConvert;
+import net.edu.ability.entity.GraduateRequireEntity;
+import net.edu.ability.query.GraduateRequireQuery;
+import net.edu.ability.service.GraduateRequireService;
+import net.edu.ability.vo.GraduateRequireVO;
+import net.edu.framework.common.page.PageResult;
+import net.edu.framework.common.utils.Result;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+* 毕业要求
+*
+* @author 阿沐 babamu@126.com
+* @since 1.0.0 2022-10-20
+*/
+@RestController
+@RequestMapping("graduateRequire")
+@Tag(name="毕业要求")
+@AllArgsConstructor
+public class GraduateRequireController {
+    private final GraduateRequireService graduateRequireService;
+
+    @GetMapping("page")
+    @Operation(summary = "分页")
+    public Result<PageResult<GraduateRequireVO>> page(@Valid GraduateRequireQuery query){
+        PageResult<GraduateRequireVO> page = graduateRequireService.page(query);
+
+        return Result.ok(page);
+    }
+
+    @GetMapping("{id}")
+    @Operation(summary = "信息")
+    public Result<GraduateRequireVO> get(@PathVariable("id") Long id){
+        GraduateRequireEntity entity = graduateRequireService.getById(id);
+        return Result.ok(GraduateRequireConvert.INSTANCE.convert(entity));
+    }
+
+    @PostMapping
+    @Operation(summary = "保存")
+    public Result<String> save(@RequestBody GraduateRequireVO vo){
+        graduateRequireService.save(vo);
+
+        return Result.ok();
+    }
+
+    @PutMapping
+    @Operation(summary = "修改")
+    public Result<String> update(@RequestBody @Valid GraduateRequireVO vo){
+        graduateRequireService.update(vo);
+
+        return Result.ok();
+    }
+
+    @DeleteMapping
+    @Operation(summary = "删除")
+    public Result<String> delete(@RequestBody List<Long> idList){
+        graduateRequireService.delete(idList);
+
+        return Result.ok();
+    }
+}
---
+++ edu-cloud-teaching/src/main/java/net/edu/ability/entity/GraduateRequireEntity.java
@@ -0,0 +1,44 @@
+package net.edu.ability.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import net.edu.framework.mybatis.entity.BaseEntity;
+
+
+/**
+ * 毕业要求
+ *
+ * @author 阿沐 babamu@126.com
+ * @since 1.0.0 2022-10-20
+ */
+@Data
+@EqualsAndHashCode(callSuper=false)
+@TableName("ability_graduate_require")
+public class GraduateRequireEntity extends BaseEntity {
+	/**
+	* 标题
+	*/
+	private String title;
+
+	/**
+	* 描述
+	*/
+	private String content;
+
+	/**
+	* 专业
+	*/
+	private Long major;
+
+	/**
+	* 学院
+	*/
+	private Long college;
+
+	/**
+	* 学校
+	*/
+	private Long school;
+
+}

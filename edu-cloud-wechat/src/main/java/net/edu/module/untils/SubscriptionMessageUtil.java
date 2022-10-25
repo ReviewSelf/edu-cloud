db8c6c648577c4439a9f20b9915fc28635459a67
skwdsb <414837845@qq.com>
2022-10-25 09:39:18 +0800

    每日更新

--- edu-cloud-wechat/src/main/java/net/edu/module/untils/SubscriptionMessageUtil.java
+++ edu-cloud-wechat/src/main/java/net/edu/module/untils/SubscriptionMessageUtil.java
@@ -78,7 +78,7 @@ public class SubscriptionMessageUtil {
         );
 
         WxMpTemplateMessage templateMessage = WxMpTemplateMessage.builder()
-                .url(WeChatProperties.EVALUATION_Url+lessonId+"/"+userId)
+                .url(WeChatProperties.EVALUATION_Url+"/"+lessonId+"/"+userId)
                 .toUser(userOpenid)
                 .templateId(OrderMsgTemplateId)
                 .data(wxMpTemplateDataList)
